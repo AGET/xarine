b23de2abd145fa3c04fe6e17da67f9eb21ea8edf
slackmart <slackmart@gmail.com>
2017-01-03 22:52:57 -0600

    El inicio

---
+++ src/main/java/org/patata/android/xarine/XarineMainActivity.java
@@ -0,0 +1,15 @@
+package org.patata.android.xarine;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class XarineMainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

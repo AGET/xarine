f07c9cdf1590a3e2b31c1b8d494d36f007aed9b0
slackmart <slackmart@gmail.com>
2017-01-06 00:43:41 -0600

    Configuracion basica del tema a usar

--- src/main/java/org/patata/android/xarine/XarineMainActivity.java
+++ src/main/java/org/patata/android/xarine/XarineMainActivity.java
@@ -1,15 +1,24 @@
 package org.patata.android.xarine;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 
-public class XarineMainActivity extends Activity
+import android.util.Log;
+
+public class XarineMainActivity extends AppCompatActivity
 {
+    private final String TAG = XarineMainActivity.class.getSimpleName();
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.xarine_toolbar);
+        setSupportActionBar(toolbar);
+        Log.i(TAG, "Activity onCreate");
     }
 }
